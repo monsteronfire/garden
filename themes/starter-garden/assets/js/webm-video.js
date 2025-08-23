97b77f87df1c477c32fb6ea7da38df6267505b94
monsteronfire <monsteronfire@gmail.com>
2025-08-23 13:46:14 +0800

    Wrap in IIFE

--- themes/starter-garden/assets/js/webm-video.js
+++ themes/starter-garden/assets/js/webm-video.js
@@ -1,17 +1,20 @@
-function isSafari() {
-    let userAgentString = navigator.userAgent;
-    // Chrome agent - needed to elinimate Chrome since Chrome browsers include both Chrome and Safari user agent string
-    let chromeAgent = userAgentString.indexOf("Chrome") > -1;
-    // Detect Safari
-    let safariAgent = userAgentString.indexOf("Safari") > -1;
+(() => {
+    function isSafari() {
+        let userAgentString = navigator.userAgent;
+        // Chrome agent - needed to elinimate Chrome since Chrome browsers include both Chrome and Safari user agent string
+        let chromeAgent = userAgentString.indexOf("Chrome") > -1;
+        // Detect Safari
+        let safariAgent = userAgentString.indexOf("Safari") > -1;
 
-    // Discard Safari since it also matches Chrome
-    if ((chromeAgent) && (safariAgent)) safariAgent = false;
+        // Discard Safari since it also matches Chrome
+        if ((chromeAgent) && (safariAgent)) safariAgent = false;
 
-    return safariAgent
-}
+        return safariAgent
+    }
 
-if (isSafari()) {
-    document.querySelector('#sitevideo-fallback').style.display = 'block';
-    document.querySelector('#sitevideo-video').style.display = 'none';
-}
+    if (isSafari()) {
+        document.querySelector('#sitevideo-fallback').style.display = 'block';
+        document.querySelector('#sitevideo-video').style.display = 'none';
+    }
+
+})()
